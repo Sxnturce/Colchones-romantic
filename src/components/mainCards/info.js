ba677e29c110b2c956ec5660eb325bc94c2f70fe
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-05-18 12:47:32 -0500

    Se acabo el section infoCards

---
+++ src/components/mainCards/info.js
@@ -0,0 +1,17 @@
+export const features = [
+  {
+    ico: "bx bx-package",
+    title: "Garantía por defectos de fabrica",
+    info: "Compre con confianza con nuestra Garantía de Satisfacción: Ámalo o recibe un reembolso."
+  },
+  {
+    ico: "bx bx-medal",
+    title: "Envios rapidos y seguros",
+    info: "¡Actualiza tu estilo hoy y obtén envío GRATIS en todos los pedidos! No te lo pierdas."
+  },
+  {
+    ico: "bx bx-check-shield",
+    title: "Compra fácil y 100% seguras",
+    info: "Su seguridad es nuestra prioridad. Tus pagos están seguros con nosotros."
+  }
+]
