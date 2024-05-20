8bd1ca73af400075c64d5e4262a988792b2966a0
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-05-20 07:47:45 -0500

    Se corrigio error con los botones add and minus

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,37 +4,37 @@ import Mobile from "./nav/Mobile.jsx";
 import { useState } from "react";
 
 export default function Header() {
-	const [move, setMove] = useState(false);
-	function changePosition() {
-		setMove(!move);
-	}
-	return (
-		<>
-			<header className="w-full fixed z-20">
-				<div className="bg-nav">
-					<h1 className="text-center  py-1 text-white">
-						Obtén un 25% de descuento en tu primer pedido
-					</h1>
-				</div>
-				<nav className="w-full bg-white">
-					<div className="nav w-11/12 max-w-7xl mx-auto flex items-center justify-between py-4">
-						<Links mobile={true} />
-						<label
-							className="block custom-lg:hidden relative z-20 burger"
-							onChange={changePosition}
-						>
-							<div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
-								<input className="hidden peer" type="checkbox" />
-								<div className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"></div>
-								<div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"></div>
-								<div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"></div>
-							</div>
-						</label>
-						<Mobile isMove={move} />
-						<Search mobile={true} />
-					</div>
-				</nav>
-			</header>
-		</>
-	);
+  const [move, setMove] = useState(false);
+  function changePosition() {
+    setMove(!move);
+  }
+  return (
+    <>
+      <header className="fixed z-20 w-full">
+        <div className="bg-nav">
+          <h1 className="py-1  text-center text-white">
+            Obtén un 25% de descuento en tu primer pedido
+          </h1>
+        </div>
+        <nav className="w-full bg-white">
+          <div className="nav mx-auto flex w-11/12 max-w-7xl items-center justify-between py-4">
+            <Links mobile={true} />
+            <label
+              className="burger relative z-20 block custom-lg:hidden"
+              onChange={changePosition}
+            >
+              <div className="flex h-10 w-9 cursor-pointer flex-col items-center justify-center">
+                <input className="peer hidden" type="checkbox" />
+                <div className="h-[2px] w-[50%] origin-left translate-y-[0.45rem] rounded-sm bg-black transition-all duration-300 peer-checked:rotate-[-45deg]"></div>
+                <div className="h-[2px] w-[50%] origin-center rounded-md bg-black transition-all duration-300 peer-checked:hidden"></div>
+                <div className="h-[2px] w-[50%] origin-left -translate-y-[0.45rem] rounded-md bg-black transition-all duration-300 peer-checked:rotate-[45deg]"></div>
+              </div>
+            </label>
+            <Mobile isMove={move} />
+            <Search mobile={true} />
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
--- src/components/product/info/Info.jsx
+++ src/components/product/info/Info.jsx
@@ -1,90 +1,90 @@
 import { useState } from "react";
 export default function Info({ title, price, stock, description, size }) {
-	const [heart, setHeart] = useState(false);
-	function changeState() {
-		setHeart(!heart);
-	}
-	const [value, setValue] = useState(0);
-	function reductValue() {
-		if (value > 0) {
-			setValue(value - 1);
-		}
-	}
-	function addValue() {
-		if (value < 20) {
-			setValue(value + 1);
-		}
-	}
-	return (
-		<>
-			<div className="flex flex-col gap-6 p-3 items-center custom-md:items-start">
-				<h3 className="sm:text-3xl text-2xl text-bgButtonHover font-bold custom-md:text-left text-center">
-					{title}
-				</h3>
-				<div className="flex items-center gap-6 ">
-					<div className="flex items-center gap-2 bg-gray-100 rounded-lg px-3 py-1">
-						<i className="bx bxs-star text-yellow-400"></i>
-						<span className="text-gray-600 text-sm">4.2-54 reviews</span>
-					</div>
-					<div
-						href="#"
-						className={`text-white font-semibold py-1 px-2 rounded-md ${
-							stock ? "bg-green-500" : "bg-red-500"
-						}`}
-					>
-						{stock ? "En stock" : "Agotado"}
-					</div>
-				</div>
-				<h4 className="text-[1.25rem] font-bold">S/ {price}</h4>
-				<p className="text-fontProduct text-center custom-md:text-left">
-					{description}
-				</p>
-				<div className="flex flex-col gap-2 items-center custom-md:items-start">
-					<h4 className="uppercase text-fontProduct text-[0.8rem]">
-						Mas Opciones disponibles
-					</h4>
-					<span className="text-neutral-900 font-semibold text-md rounded-md border-[1px] px-4 py-[0.20rem] border-black max-w-max">
-						{size}
-					</span>
-				</div>
-				<div className="flex flex-col gap-3">
-					<h3 className="uppercase text-fontProduct text-[0.8rem] text-center custom-md:text-left ">
-						Cantidad
-					</h3>
-					<div className="flex text-[1rem] text-fontProduct font-semibold ">
-						<span
-							className="cursor-pointer border-[1px] border-[#E6E7E8] py-1 px-4 rounded-l rounded-tl rounded-bl hover:bg-red-500 hover:border-red-500 hover:text-white transition-all ease-in-out duration-200 flex items-center justify-center"
-							onClick={reductValue}
-						>
-							<i className="fa-solid fa-minus"></i>
-						</span>
-						<span className="border-t-[1px] border-b-[1px] text-lg  border-[#E6E7E8] py-1 px-4 ">
-							{value}
-						</span>
-						<span
-							className="cursor-pointer border-[1px] border-[#E6E7E8] py-1 px-4 rounded-r rounded-tr rounded-br hover:bg-green-600 hover:border-green-600 hover:text-white transition-all ease-in-out duration-200 flex items-center justify-center"
-							onClick={addValue}
-						>
-							<i className="fa-solid fa-plus"></i>
-						</span>
-					</div>
-				</div>
-				<div className="flex gap-4">
-					<button className="bg-nav text-white py-2  px-8 custom-md:px-20 rounded-md hover:bg-red-500 transition-all ease-in-out duration-200">
-						Añadir al carrito
-					</button>
-					<div
-						className="flex items-center justify-center cursor-pointer py-1 px-2 rounded-sm border-2 ring-red-400"
-						onClick={changeState}
-					>
-						<i
-							className={`bx ${
-								heart ? "bxs-heart text-red-700" : "bx-heart"
-							} text-xl hover:text-red-700`}
-						></i>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const [heart, setHeart] = useState(false);
+  function changeState() {
+    setHeart(!heart);
+  }
+  const [value, setValue] = useState(0);
+  function reductValue() {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  }
+  function addValue() {
+    if (value < 20) {
+      setValue(value + 1);
+    }
+  }
+  return (
+    <>
+      <div className="flex flex-col items-center gap-6 p-3 custom-md:items-start">
+        <h3 className="text-center text-2xl font-bold text-bgButtonHover sm:text-3xl custom-md:text-left">
+          {title}
+        </h3>
+        <div className="flex items-center gap-6 ">
+          <div className="flex items-center gap-2 rounded-lg bg-gray-100 px-3 py-1">
+            <i className="bx bxs-star text-yellow-400"></i>
+            <span className="text-sm text-gray-600">4.2-54 reviews</span>
+          </div>
+          <div
+            href="#"
+            className={`rounded-md px-2 py-1 font-semibold text-white ${
+              stock ? "bg-green-500" : "bg-red-500"
+            }`}
+          >
+            {stock ? "En stock" : "Agotado"}
+          </div>
+        </div>
+        <h4 className="text-[1.25rem] font-bold">S/ {price}</h4>
+        <p className="text-center text-fontProduct custom-md:text-left">
+          {description}
+        </p>
+        <div className="flex flex-col items-center gap-2 custom-md:items-start">
+          <h4 className="text-[0.8rem] uppercase text-fontProduct">
+            Mas Opciones disponibles
+          </h4>
+          <span className="text-md max-w-max rounded-md border-[1px] border-black px-4 py-[0.20rem] font-semibold text-neutral-900">
+            {size}
+          </span>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h3 className="text-center text-[0.8rem] uppercase text-fontProduct custom-md:text-left ">
+            Cantidad
+          </h3>
+          <div className="flex text-[1rem] font-semibold text-fontProduct ">
+            <span
+              className="flex cursor-pointer select-none items-center justify-center rounded-l rounded-bl rounded-tl border-[1px] border-[#E6E7E8] px-4 py-1 text-xl transition-all duration-200 ease-in-out hover:border-red-500 hover:bg-red-500 hover:text-white"
+              onClick={reductValue}
+            >
+              -
+            </span>
+            <span className="border-b-[1px] border-t-[1px] border-[#E6E7E8]  px-4 py-1 text-lg ">
+              {value}
+            </span>
+            <span
+              className="flex cursor-pointer select-none items-center justify-center rounded-r rounded-br rounded-tr border-[1px] border-[#E6E7E8] px-4 py-1 text-xl transition-all duration-200 ease-in-out hover:border-green-600 hover:bg-green-600 hover:text-white"
+              onClick={addValue}
+            >
+              +
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <button className="rounded-md bg-nav px-8  py-2 text-white transition-all duration-200 ease-in-out hover:bg-red-500 custom-md:px-20">
+            Añadir al carrito
+          </button>
+          <div
+            className="flex cursor-pointer items-center justify-center rounded-sm border-2 px-2 py-1 ring-red-400"
+            onClick={changeState}
+          >
+            <i
+              className={`bx ${
+                heart ? "bxs-heart text-red-700" : "bx-heart"
+              } text-xl hover:text-red-700`}
+            ></i>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
